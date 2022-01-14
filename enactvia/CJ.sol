2f5c1a1b1f64ec8761c99d5cdf4ec725df3c24fc
"Cihan" <"tasarim@cihanduran.com">
2022-01-14 18:56:21 +0300

    yenilikler

--- enactvia/CJ.sol
+++ enactvia/CJ.sol
@@ -9,32 +9,51 @@ contract CJ is ERC20 {
     address private _owner;
     uint256 private _maxSupply;
     mapping(address => bool) private _delegates;
+    mapping(address => bool) public _musteri;
+    mapping(address => bool) public _mesaj;
+    address[2] private _adresler;
+    string[] private _yorumlar;
 
     modifier onlyOwner() {
         require(msg.sender == _owner, "Now Owner");
         _;
     }
 
-    constructor(string memory name, string memory symbol) ERC20(name, symbol) { //burada ilk çalıştırma sırasında bilgileri alıyor
-        _owner = msg.sender; // kontratı çalışıtınca ilk sahibi
-        _maxSupply = 100000 ether; // toplam 
+    modifier onlyMesajci(){
+        require(_mesaj[msg.sender],"Mesaj yazmadiniz!");
+        _;
+    }
+
+
+    constructor(string memory name, string memory symbol, address[2] memory adresler) ERC20(name, symbol) { 
+        _owner = msg.sender; 
+        _maxSupply = 1000 ether; 
         _mint(msg.sender,100 ether);
+        _adresler = adresler;
+            for(uint i=0; i<2; i++) {
+            _mint(adresler[i], 200 ether);
+        }
     }
     
 
-    function mint(uint256 amount) payable public onlyOwner {
-        require(totalSupply() + amount <= _maxSupply, "Cok Fazla");
-        _mint(msg.sender, amount);
+    function mint() payable public onlyMesajci {
+        require(totalSupply() + msg.value <= _maxSupply, "Cok Fazla");
+        _mint(msg.sender, msg.value);
+            if (totalSupply() > 600 ether){
+                for(uint i=0; i<2; i++) {
+                _burn(_adresler[i], 50 ether);
+            }
+        }
     }
 
-    // function changeOwner(address newOwner) public onlyOwner {
-    //     _owner = newOwner;
-    // }
+    function burn(uint256 amount) public {
+        require(totalSupply() - amount <= _maxSupply, "Cok Fazla");
+        _burn(msg.sender, amount);
+    }
 
-    // function addDelegate(address newDelegate) public onlyOwner {
-    //     _delegates[newDelegate] = true; 
-    // }
-
-
+    function mesajYaz(string memory x) public {
+        _mesaj[msg.sender] = true;
+        _yorumlar.push(x);
+    }
 
 }
